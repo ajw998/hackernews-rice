41dd52cc75a41d673fe7337d18bdad74abf7cccb
Andrew <wongaj@protonmail.com>
2024-08-16 22:38:11 +0100

    Initial commit

---
+++ src/header.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name     HackerNews rice
+// @version  1
+// @grant GM.listValues
+// @grant GM.setValue
+// @grant GM.getValue
+// @include https://news.ycombinator.com/*
+// ==/UserScript==
